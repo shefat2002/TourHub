6d9b9ab0f7a8703aba0840c64e0d47570c86494d
Shefat Al Mahmud <shefat2002@gmail.com>
2026-03-10 12:14:03 +0600

    Add: YARP implementation

--- TourHub.ApiGateway/Program.cs
+++ TourHub.ApiGateway/Program.cs
@@ -1,23 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.WebHost.UseUrls("http://localhost:5000");
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapReverseProxy();
 
 app.Run();
